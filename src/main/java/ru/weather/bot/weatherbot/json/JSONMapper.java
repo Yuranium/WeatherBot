9ee2d3088b92e40dd9765f9d8099f8d84c8efca4
Yuranium <dolazifet@gmail.com>
2024-04-20 02:07:35 +0300

    Продолжение работы над реализацией REST для парсинга погоды.

--- src/main/java/ru/weather/bot/weatherbot/json/JSONMapper.java
+++ src/main/java/ru/weather/bot/weatherbot/json/JSONMapper.java
@@ -1,18 +1,73 @@
 package ru.weather.bot.weatherbot.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class JSONMapper
 {
     @Value("${weather.api.key}")
-    private String weatherAPIKey;
+    private String WEATHER_API_KEY;
 
     @Value("${url.template}")
-    private String templateURL;
-    private JSONWeather weather;
+    private String TEMPLATE_URL;
+    private WeatherData weatherData;
 
-    public boolean isCity(String text)
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        try (scanner)
+        {
+            System.out.println("Введите город, погоду которого хотите узнать:");
+            String city = scanner.nextLine();
+            WeatherData weatherData;
+            weatherData = fetchWeather(city);
+            System.out.println("В городе " + convertCityNameCorrectly(city) + " " + weatherData.weather().get(0).description() + ", средняя температура на улице "
+                    + weatherData.main().temp() + "°C, ощущается как " + weatherData.main().feels_like() +
+                    "°C, облачность составляет " + weatherData.clouds().clouds() + "%, порывы ветра достигают " + weatherData.wind().speed() + " м/c.");
+            System.out.println(weatherData);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private String convertCityNameCorrectly(String str)
     {
-        return false;
+        if (str == null || str.isEmpty())
+            throw new IllegalArgumentException("Invalid string 'str' passed in upperFirstChar(String str) method, string does not exist or it is empty");
+        int index = str.indexOf("-");
+        return str.contains("-") ? str.substring(0, 1).toUpperCase() + str.substring(1, index + 1) + str.substring(index + 1, index + 2).toUpperCase()
+                + str.substring(index + 2) : str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    private WeatherData fetchWeather(String city) throws IOException {
+        String urlString = TEMPLATE_URL.replace("{city name}", city).replace("{my lang}", "ru")
+                .replace("{units}", "metric").replace("{API key}", WEATHER_API_KEY);
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        // Получение ответа
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK)
+        {
+            // Чтение ответа
+            StringBuilder response = new StringBuilder();
+            try (Scanner scanner = new Scanner(connection.getInputStream()))
+            {
+                while (scanner.hasNextLine())
+                    response.append(scanner.nextLine());
+            }
+
+            // Преобразование JSON в объект WeatherData с помощью Jackson
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(response.toString(), WeatherData.class);
+        }
+        else throw new RuntimeException("HTTP Response Code: " + responseCode);
     }
 }
---
+++ src/main/java/ru/weather/bot/weatherbot/json/WeatherData.java
@@ -0,0 +1,122 @@
+package ru.weather.bot.weatherbot.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record WeatherData(
+        List<Weather> weather,
+        Main main,
+        double visibility,
+        Wind wind,
+        Clouds clouds
+)
+{
+    @Override
+    public String toString()
+    {
+        return "WeatherData{" +
+                "weather=" + weather +
+                ", main=" + main +
+                ", visibility=" + visibility +
+                ", wind=" + wind +
+                ", clouds=" + clouds +
+                '}';
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+record Weather(
+        String main,
+        String description
+)
+{
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "main='" + main + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+record Main(
+        double temp,
+        double feels_like,
+        double temp_min,
+        double temp_max,
+        double pressure,
+        double humidity,
+        double sea_level,
+        double grnd_level
+)
+{
+    @Override
+    public String toString() {
+        return "Main{" +
+                "temp=" + temp +
+                ", feels_like=" + feels_like +
+                ", temp_min=" + temp_min +
+                ", temp_max=" + temp_max +
+                ", pressure=" + pressure +
+                ", humidity=" + humidity +
+                ", sea_level=" + sea_level +
+                ", grnd_level=" + grnd_level +
+                '}';
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+record Wind(
+        double speed,
+        double deg,
+        double gust
+)
+{
+    public String windGustFinding()
+    {
+        if ((deg <= 360 && deg >= 350) || (deg <= 10 && deg >= 0))
+            return "Северное направление ветра";
+        if (deg <= 100 && deg >= 80)
+            return "Восточное направление ветра";
+        if (deg <= 190 && deg >= 170)
+            return "Южное направление ветра";
+        if (deg <= 280 && deg >= 260)
+            return "Западное направление ветра";
+        if (deg > 10 && deg < 80)
+            return "Северо-восточное направление ветра";
+        if (deg > 100 && deg < 170)
+            return "Юго-восточное направление ветра";
+        if (deg > 190 && deg < 260)
+            return "Юго-западное направление ветра";
+        if (deg > 280 && deg < 350)
+            return "Северо-западное направление ветра";
+        else throw new IllegalArgumentException("Invalid value of the 'deg' variable. The range of this variable is {0, 360}");
+    }
+
+    @Override
+    public String toString() {
+        return "wind{" +
+                "speed=" + speed +
+                ", deg=" + deg +
+                ", gust=" + gust +
+                '}';
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+record Clouds(
+        @JsonProperty("all")
+        int clouds
+)
+{
+    @Override
+    public String toString() {
+        return "clouds{" +
+                "all=" + clouds +
+                '}';
+    }
+}
